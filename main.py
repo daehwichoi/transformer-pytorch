057ab1a08a7575040ab812045ee86948faa31b73
daehwichoi <48538226+daehwichoi@users.noreply.github.com>
2023-07-15 10:50:38 +0900

    [Feature] 파일  구조 생성

---
+++ main.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn
+
+from model.transformer import Transformer
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ model/transformer.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class Transformer(nn.Module):
+    def __int__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
