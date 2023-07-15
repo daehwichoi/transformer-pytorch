8f2b2b191ad012554318b1698fe3bce86ffb56e4
daehwichoi <48538226+daehwichoi@users.noreply.github.com>
2023-07-15 10:52:02 +0900

    [Feature] multi-head attention, feed forward  구조 생성

--- model/transformer.py
+++ model/transformer.py
@@ -2,6 +2,22 @@ import torch
 import torch.nn as nn
 
 
+class MultiHeadAttention(nn.Module):
+    def __int__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
+class FeedForward(nn.Module):
+    def __int__(self):
+        super().__init__()
+
+    def forward(self):
+        pass
+
+
 class Transformer(nn.Module):
     def __int__(self):
         super().__init__()
